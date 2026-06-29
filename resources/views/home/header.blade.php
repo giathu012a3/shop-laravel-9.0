51c3edf7e88f6b1719cca22370d192b186cbab6a
Nguyen Za Thu <nguyengiathu24052004@gmail.com>
2026-06-29 21:12:22 +0700

    fix(ui): correct navbar collapse class typo and style search bar

--- resources/views/home/header.blade.php
+++ resources/views/home/header.blade.php
@@ -1,13 +1,12 @@
 <header class="header_section">
     <div class="container">
         <nav class="navbar navbar-expand-lg custom_nav-container ">
-            <a class="navbar-brand" href="{{ url('/') }}"><img height="90px" src="/images/logo.png"
-                    alt="#" /></a>
+            <a class="navbar-brand" href="{{ url('/') }}"><img height="90px" src="/images/logo.png" alt="#" /></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class=""> </span>
             </button>
-            <div class="collaps e navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
                         <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
@@ -26,32 +25,22 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('view_all_product') }}">Products</a>
                     </li>
-
-
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('show_cart') }}">Cart</a>
                     </li>
-
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('show_order') }}">Order</a>
                     </li>
-
-
-
-
                     @if (Route::has('login'))
                         @auth
                             <li class="nav-item">
                                 <x-app-layout>
-
                                 </x-app-layout>
-
                             </li>
                         @else
                             <li class="nav-item">
                                 <a class="btn btn-primary" id="logincss" href="{{ route('login') }}">Đăng Nhập</a>
                             </li>
-
                             <li class="nav-item">
                                 <a class="btn btn-success" href="{{ route('register') }}">Đăng Ký</a>
                             </li>
@@ -59,18 +48,17 @@
                     @endif
                 </ul>
             </div>
-
         </nav>
-
     </div>
-
-    <div>
-        <form action="{{ url('product_search') }}" method="GET">
+    <div class="container mt-2">
+        <form action="{{ url('product_search') }}" method="GET" class="form-inline justify-content-center">
             @csrf
-            <input style="text-transform: none !important;width: 600px;margin-left: 35% ;" type="text" name="search"
-                placeholder="Bạn có thể tìm kiếm sản phẩm ở đây">
-
-            <input type="submit" value="search">
+            <div class="input-group style-search-bar" style="width: 100%; max-width: 600px;">
+                <input style="text-transform: none !important;" type="text" name="search" class="form-control" placeholder="Bạn có thể tìm kiếm sản phẩm ở đây">
+                <div class="input-group-append">
+                    <button class="btn btn-danger" type="submit">Tìm kiếm</button>
+                </div>
+            </div>
         </form>
     </div>
     <script>
@@ -78,7 +66,6 @@
             var scrollpos = localStorage.getItem('scrollpos');
             if (scrollpos) window.scrollTo(0, scrollpos);
         });
-
         window.onbeforeunload = function(e) {
             localStorage.setItem('scrollpos', window.scrollY);
         };
